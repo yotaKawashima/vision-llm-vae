e5356ec13212bd293554d26edc0a182fcea977d4
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-03-08 15:46:46 +0100

    Update: config

--- config.py
+++ config.py
@@ -26,13 +26,13 @@ batch_size = 128
 learning_rate = 0.0005
 clip_grad_norm = 1.0
 ###### Model ######
-model_type = "encoder"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
+model_type = "ae"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
 resnet_flag = False
 latent_dim = text_embedding_dim
-checkpoint_path = None
-# checkpoint_path = Path(
-#     "/mnt/data/checkpoints/resnet18_encoder_loss_norm_and_cosine_similarity_smoothL1_alpha10.0/cocoDoerig/run_0/checkpoint_epoch30.ckpt"
-# )  # or None
+# checkpoint_path = None
+checkpoint_path = Path(
+    "/mnt/data/checkpoints/encoder_loss_norm_and_cosine_similarity_smoothL1_alpha10.0/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
+)  # or None
 encoder_checkpoint = True  # whether to initialize the encoder with the checkpoint from the encoder model (only applicable for ae model)
 ae_checkpoint = False  # whether to initialize the ae model with the checkpoint from the ae model (only applicable for beta_vae model)
 vae_checkpoint = False  # whether to initialize the vae model with the checkpoint from the vae model (only applicable for beta_vae_llm model)
@@ -106,46 +106,65 @@ img_mean = [0.485, 0.456, 0.406]
 img_std = [0.229, 0.224, 0.225]
 
 
-# PIL image
-img_transform = T.Compose(
-    [
-        T.Resize((img_resize, img_resize)),
-        T.ToTensor(),
-        T.Normalize(mean=img_mean, std=img_std),
-    ]
-)
-img_transform_augmentation = T.Compose(
-    [
-        T.Resize((img_resize, img_resize)),
-        T.RandomAffine(degrees=2, translate=(0.05, 0.05)),
-        T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
-        T.RandomGrayscale(p=0.05),
-        T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
-        T.ToTensor(),
-        T.Normalize(mean=img_mean, std=img_std),
-    ]
-)
+if model_type in ["encoder"]:
+    # PIL image
+    img_transform_val = T.Compose(
+        [
+            T.Resize((img_resize, img_resize)),
+            T.ToTensor(),
+            T.Normalize(mean=img_mean, std=img_std),
+        ]
+    )
+    img_transform_train = T.Compose(
+        [
+            T.Resize((img_resize, img_resize)),
+            T.RandomAffine(degrees=2, translate=(0.05, 0.05)),
+            T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            T.RandomGrayscale(p=0.05),
+            T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
+            T.ToTensor(),
+            T.Normalize(mean=img_mean, std=img_std),
+        ]
+    )
+    # h5: array of uint8 in HWC format, need to be converted to float CHW and normalized.
+    img_transform_val_h5 = T.Compose(
+        [
+            T.ToTensor(),  # convert uint8 HWC [0, 255] to float CHW [0.0, 1.0]
+            T.Resize((img_resize, img_resize)),
+            T.Normalize(mean=img_mean, std=img_std),
+        ]
+    )
+    img_transform_train_h5 = T.Compose(
+        [
+            T.ToTensor(),  # convert uint8 HWC [0, 255] to float CHW [0.0, 1.0]
+            T.Resize((img_resize, img_resize)),
+            T.RandomAffine(degrees=2, translate=(0.05, 0.05)),
+            T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
+            T.RandomGrayscale(p=0.05),
+            T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
+            T.Normalize(mean=img_mean, std=img_std),
+        ]
+    )
 
-# h5: array of uint8 in HWC format, need to be converted to float CHW and normalized.
-img_transform_h5 = T.Compose(
-    [
-        T.ToTensor(),  # convert uint8 HWC [0, 255] to float CHW [0.0, 1.0]
-        T.Resize((img_resize, img_resize)),
-        T.Normalize(mean=img_mean, std=img_std),
-    ]
-)
-img_transform_augmentation_h5 = T.Compose(
-    [
-        T.ToTensor(),  # convert uint8 HWC [0, 255] to float CHW [0.0, 1.0]
-        T.Resize((img_resize, img_resize)),
-        T.RandomAffine(degrees=2, translate=(0.05, 0.05)),
-        T.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1),
-        T.RandomGrayscale(p=0.05),
-        T.GaussianBlur(kernel_size=(3, 3), sigma=(0.1, 2.0)),
-        T.Normalize(mean=img_mean, std=img_std),
-    ]
-)
+else:  # no data augmentation for image reconstruction models
+    # PIL image
+    img_transform_val = T.Compose(
+        [
+            T.Resize((img_resize, img_resize)),
+            T.ToTensor(),
+            T.Normalize(mean=img_mean, std=img_std),
+        ]
+    )
+    img_transform_train = img_transform_val
 
+    # h5
+    img_transform_val_h5 = T.Compose(
+        [
+            T.ToTensor(),
+            T.Resize((img_resize, img_resize)),
+        ]
+    )
+    img_transform_train_h5 = img_transform_val_h5
 
 ############################################################################
 
