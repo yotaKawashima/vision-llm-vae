39d1d6f70e814c22ad63ca720cbb293861478438
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-03-11 18:47:20 +0100

    update: beta

--- config.py
+++ config.py
@@ -55,7 +55,7 @@ MODEL_CONFIGS = {
     },
     "beta_vae": {
         "loss_type": "llm_alignment",  # or "standard"
-        "beta": 0.1,  # beta for KL divergence loss
+        "beta": 0.001,  # beta for KL divergence loss
         "recon_loss_type": "l2",
         "llm_alignment_loss_type": "cosine_similarity",
         "gamma": 1.0,  # weight for llm alignment loss when loss_type is "llm_alignment"
