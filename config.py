61a8133bdb3489e4285918f43ecb556a137e54e8
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-03-13 18:30:43 +0100

    Update: custom ae with pre-training

--- config.py
+++ config.py
@@ -26,14 +26,14 @@ batch_size = 128
 learning_rate = 0.0005
 clip_grad_norm = 1.0
 ###### Model ######
-model_type = "encoder"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
+model_type = "ae"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
 resnet_flag = False
 latent_dim = text_embedding_dim
-checkpoint_path = None
-# checkpoint_path = Path(
-#     "/mnt/data/checkpoints/encoder_loss_norm_and_cosine_similarity_smoothL1_alpha10.0/cocoDoerig/run_0/checkpoint_epoch40.ckpt"
-# )  # or None
-encoder_checkpoint = False  # whether to initialize the encoder with the checkpoint from the encoder model (only applicable for ae model)
+# checkpoint_path = None
+checkpoint_path = Path(
+    "/mnt/data/checkpoints/encoder_loss_norm_and_cosine_similarity_smoothL1_alpha10.0/cocoDoerig/run_0/checkpoint_epoch20.ckpt"
+)  # or None
+encoder_checkpoint = True  # whether to initialize the encoder with the checkpoint from the encoder model (only applicable for ae model)
 ae_checkpoint = False  # whether to initialize the ae model with the checkpoint from the ae model (only applicable for beta_vae model)
 vae_checkpoint = False  # whether to initialize the vae model with the checkpoint from the vae model (only applicable for beta_vae_llm model)
 last_checkpoint_path = None  # for activation extraction
@@ -58,7 +58,7 @@ MODEL_CONFIGS = {
         "beta": 0.001,  # beta for KL divergence loss
         "recon_loss_type": "l2",
         "llm_alignment_loss_type": "cosine_similarity",
-        "gamma": 5.0,  # weight for llm alignment loss when loss_type is "llm_alignment"
+        "gamma": 10.0,  # weight for llm alignment loss when loss_type is "llm_alignment"
     },
     "beta_vae_llm": {
         "loss_type": "l2",
