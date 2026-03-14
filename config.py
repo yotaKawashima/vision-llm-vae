06225f4548d087728356e2c8c40009e85c02d681
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-03-14 17:39:59 +0100

    Update: llm alignment betavae

--- config.py
+++ config.py
@@ -26,15 +26,15 @@ batch_size = 128
 learning_rate = 0.0005
 clip_grad_norm = 1.0
 ###### Model ######
-model_type = "decoder"  # "encoder", "ae", "beta_vae", "beta_vae_llm", "decoder"
-resnet_flag = True
+model_type = "beta_vae"  # "encoder", "ae", "beta_vae", "beta_vae_llm", "decoder"
+resnet_flag = False
 latent_dim = text_embedding_dim
-checkpoint_path = None
-# checkpoint_path = Path(
-#     "/mnt/data/checkpoints/vanilla_ae_loss_l2/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
-# )  # or None
+# checkpoint_path = None
+checkpoint_path = Path(
+    "/mnt/data/checkpoints/ae_loss_l2/cocoDoerig/run_0/checkpoint_epoch30.ckpt"
+)  # or None
 encoder_checkpoint = False  # whether to initialize the encoder with the checkpoint from the encoder model (only applicable for ae model)
-ae_checkpoint = False  # whether to initialize the ae model with the checkpoint from the ae model (only applicable for beta_vae model)
+ae_checkpoint = True  # whether to initialize the ae model with the checkpoint from the ae model (only applicable for beta_vae model)
 vae_checkpoint = False  # whether to initialize the vae model with the checkpoint from the vae model (only applicable for beta_vae_llm model)
 last_checkpoint_path = None  # for activation extraction
 loss_type = None
@@ -54,11 +54,11 @@ MODEL_CONFIGS = {
         "loss_type": "l2",
     },
     "beta_vae": {
-        "loss_type": "standard",  # or "llm_alignment"
+        "loss_type": "llm_alignment",  # or "llm_alignment"
         "beta": 0.001,  # beta for KL divergence loss
         "recon_loss_type": "l2",
         "llm_alignment_loss_type": "cosine_similarity",
-        "gamma": 10.0,  # weight for llm alignment loss when loss_type is "llm_alignment"
+        "gamma": 1.0,  # weight for llm alignment loss when loss_type is "llm_alignment"
     },
     "beta_vae_llm": {
         "loss_type": "l2",
@@ -99,6 +99,7 @@ elif model_type == "beta_vae":
             full_model_name = "vanilla_from_ae_" + full_model_name
         else:
             full_model_name = "vanilla_" + full_model_name
+
 
 elif model_type == "ae":
     if not encoder_checkpoint:
