1e7be63475c88da5f02ad09b2a86d63a8dd351c6
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-03-16 16:41:02 +0100

    Update: betavae gamma 0.1

--- config.py
+++ config.py
@@ -58,7 +58,7 @@ MODEL_CONFIGS = {
         "beta": 0.001,  # beta for KL divergence loss
         "recon_loss_type": "l2",
         "llm_alignment_loss_type": "cosine_similarity",
-        "gamma": 0.25,  # weight for llm alignment loss when loss_type is "llm_alignment"
+        "gamma": 0.1,  # weight for llm alignment loss when loss_type is "llm_alignment"
     },
     "beta_vae_llm": {
         "loss_type": "l2",
