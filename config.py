639c7fccc002409650bf134ad23c49d3b9056af3
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-06-16 16:07:01 +0200

    update: config for standard vae evaluation

--- config.py
+++ config.py
@@ -30,13 +30,13 @@ resnet_flag = False
 eval_flag = True  # true to make relevant dirs for evaluation and activation extraction
 latent_dim = text_embedding_dim
 # checkpoint_path = None
-checkpoint_path = Path(
-    "/mnt/data/checkpoints/beta_vae_loss_llm_alignment_beta0.001_recon_loss_l2_llm_alignment_loss_cosine_similarity_gamma0.5/cocoDoerig/run_0/checkpoint_epoch30.ckpt"
-)
-
 # checkpoint_path = Path(
-#     "/mnt/data/checkpoints/vanilla_from_ae_beta_vae_loss_standard_beta0.001_recon_loss_l2/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
+#     "/mnt/data/checkpoints/beta_vae_loss_llm_alignment_beta0.001_recon_loss_l2_llm_alignment_loss_cosine_similarity_gamma0.5/cocoDoerig/run_0/checkpoint_epoch30.ckpt"
 # )
+
+checkpoint_path = Path(
+    "/mnt/data/checkpoints/vanilla_from_ae_beta_vae_loss_standard_beta0.001_recon_loss_l2/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
+)
 
 # checkpoint_path = Path(
 #     "/mnt/data/checkpoints/vanilla_ae_loss_l2/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
@@ -48,10 +48,10 @@ checkpoint_path = Path(
 
 encoder_checkpoint = False  # whether to initialize the encoder with the checkpoint from the encoder model
 ae_checkpoint = (
-    True  # whether to initialize the ae model with the checkpoint from the ae model
+    False  # whether to initialize the ae model with the checkpoint from the ae model
 )
 vae_checkpoint = (
-    False  # whether to initialize the vae model with the checkpoint from the vae model
+    True  # whether to initialize the vae model with the checkpoint from the vae model
 )
 loss_type = None
 beta = None
@@ -73,7 +73,7 @@ MODEL_CONFIGS = {
         "loss_type": "l2",
     },
     "beta_vae": {
-        "loss_type": "llm_alignment",  # or "llm_alignment"
+        "loss_type": "standard",  # or "llm_alignment"
         "beta": 0.001,  # beta for KL divergence loss
         "recon_loss_type": "l2",
         "llm_alignment_loss_type": "cosine_similarity",
