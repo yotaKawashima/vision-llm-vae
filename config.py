eb4eb5d84a9c165e533e5edcdb555ec9853e051c
Yohta Kawashima <yohta.kawashima@hhi.fraunhofer.de>
2026-06-25 18:25:44 +0200

    update: config for beta vae llm recon eval

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ batch_size = 128
 learning_rate = 0.0005
 clip_grad_norm = 1.0
 ###### Model ######
-model_type = "decoder"  # "beta_vae"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
+model_type = "beta_vae_llm"  # "decoder"  # "beta_vae"  # "encoder", "ae", "beta_vae", "beta_vae_llm"
 resnet_flag = False
 eval_flag = True  # true to make relevant dirs for evaluation and activation extraction
 latent_dim = text_embedding_dim
@@ -42,8 +42,11 @@ latent_dim = text_embedding_dim
 # checkpoint_path = Path(
 #     "/mnt/data/checkpoints/ae_loss_l2/cocoDoerig/run_0/checkpoint_epoch25.ckpt"
 # )
+# checkpoint_path = Path(
+#     "/mnt/data/checkpoints/decoder_loss_l2/cocoDoerig/run_0/checkpoint_epoch10.ckpt"
+# )
 checkpoint_path = Path(
-    "/mnt/data/checkpoints/decoder_loss_l2/cocoDoerig/run_0/checkpoint_epoch10.ckpt"
+    "/mnt/data/checkpoints/beta_vae_llm_loss_l2_and_img_norm_delta0.1/cocoDoerig/run_0/cocoDoerig/run_0/checkpoint_epoch50.ckpt"
 )
 
 encoder_checkpoint = False  # whether to initialize the encoder with the checkpoint from the encoder model
